df77a2b8df5b17a7a2fa06b8298be8ba7a07530d
AMIR ESHAQ <31240313+jamalag@users.noreply.github.com>
2021-09-13 16:50:49 -0400

    small change

--- example1/app.js
+++ example1/app.js
@@ -71,7 +71,7 @@ const createWorker = async () => {
 // We create a Worker as soon as our application starts
 worker = createWorker()
 
-// These is an Array of RtpCapabilities
+// This is an Array of RtpCapabilities
 // https://mediasoup.org/documentation/v3/mediasoup/rtp-parameters-and-capabilities/#RtpCodecCapability
 // list of media codecs supported by mediasoup ...
 // https://github.com/versatica/mediasoup/blob/v3/src/supportedRtpCapabilities.ts
